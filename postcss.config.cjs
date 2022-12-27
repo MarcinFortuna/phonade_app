b9abf9ceac696bc7eccc41b38f6efd66e78e2540
Marcin Fortuna <marcin.fort@gmail.com>
2022-12-27 23:34:49 +0100

    Fix configs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,19 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+    plugins: {
+        tailwindcss: {
+            content: [
+                "./index.html",
+                "./src/*.{js,ts,jsx,tsx}",
+                "./src/**/*.{js,ts,jsx,tsx}",
+            ],
+            theme: {
+                extend: {},
+            },
+            plugins: [require('daisyui')],
+            daisyui: {
+                themes: ["dark", "light"],
+            }
+        },
+        autoprefixer: {},
+    },
 }
