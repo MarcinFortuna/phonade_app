0852e367e44ace65fb714f742ba0c87c72d32479
Marcin Fortuna <marcin.fort@gmail.com>
2023-01-24 23:33:53 +0100

    Add a full list of transcriptions to browse

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Info from "./Info";
 import ThemeChangeToggle from "./ThemeChangeToggle";
 import {UseIpaToggle} from "./UseIpaToggle";
+import {SeeList} from "./SeeList";
 
 function Header() {
     return (<header className="navbar bg-base-100 flex justify-between">
         <div className="btn btn-ghost normal-case text-xl">phonade</div>
-        <div className="flex gap-1">
+        <div className="flex gap-4">
             <UseIpaToggle />
+            <SeeList />
             <Info />
             <ThemeChangeToggle />
         </div>
