ad020c33a2ff6539e11f818eea27cba9ce8cf7c8
Marcin Fortuna <marcin.fort@gmail.com>
2023-02-04 19:45:51 +0100

    add transcriptions for inflected words

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,6 +1,14 @@
-export type transcription = {
+export type transcription_most_common = {
     id: number,
     spelling: string,
     sampa_strong: string,
     sampa_weak: string
+};
+
+export type transcription_infl = {
+    id: number,
+    basic: string,
+    infl: string,
+    sampa_basic: string,
+    sampa_infl: string
 };
